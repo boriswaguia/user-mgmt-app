70bc6ab11ec6ee93ff755a2d92a2d4b5983e7be6
Boris W. Waguia <bwa@adorsys.de>
2021-02-23 12:14:19 +0100

    feat: Add user template form

--- user-mgmt-app/src/app/users/user-dialog/user-dialog.component.ts
+++ user-mgmt-app/src/app/users/user-dialog/user-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { User } from '../domain/user.domain'
 
 @Component({
     selector: 'app-user-dialog',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./user-dialog.component.scss'],
 })
 export class UserDialogComponent implements OnInit {
+
+    user: User = {
+      firstName: '',
+      lastName: '',
+      login: '',
+      password: ''
+    };
+
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    saveForm(): void {
+      console.log('first name', this.user);
+    }
+
+    dismissDialog(): void {}
+    closeModal(): void {}
 }
--- user-mgmt-app/src/app/users/users.module.ts
+++ user-mgmt-app/src/app/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersRoutingModule } from './users-routing.module'
 import { UsersComponent } from './users/users.component'
 import { NgxDatatableModule } from '@swimlane/ngx-datatable'
 import { UserDialogComponent } from './user-dialog/user-dialog.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
     declarations: [UsersComponent, UserDialogComponent],
-    imports: [CommonModule, UsersRoutingModule, NgxDatatableModule],
+    imports: [CommonModule, UsersRoutingModule, NgxDatatableModule, FormsModule],
 })
 export class UsersModule {}
