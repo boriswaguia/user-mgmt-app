4f4f0fea11503d7fd045a09b480bb63e9b922657
Boris W. Waguia <bwa@adorsys.de>
2021-02-23 10:12:56 +0100

    Add routing to users module

--- user-mgmt-app/src/app/app.component.ts
+++ user-mgmt-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -8,8 +9,9 @@ import { Component } from '@angular/core'
 export class AppComponent {
     title = 'user-mgmt-app'
 
+    constructor(private router: Router) {}
 
     openUserManagementUI(): void {
-
+      this.router.navigate(["/users"]);
     }
 }
