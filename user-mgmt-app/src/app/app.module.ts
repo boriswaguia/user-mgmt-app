9047d9fd92ba0eda74c8720502358065fde2b058
Boris W. Waguia <bwa@adorsys.de>
2021-02-23 10:41:01 +0100

    Add routing

--- user-mgmt-app/src/app/app.module.ts
+++ user-mgmt-app/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeRoutingModule } from './home/home-routing.module'
 import { UsersModule } from './users/users.module'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, UsersModule],
+    imports: [BrowserModule, AppRoutingModule, HomeRoutingModule, UsersModule],
     providers: [],
     bootstrap: [AppComponent],
 })
