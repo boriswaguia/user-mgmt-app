f8163bbfa1b41f713c678f3e202199f16556f7b8
Boris W. Waguia <bwa@adorsys.de>
2021-02-24 11:47:01 +0100

    test: Test UserService#get

--- user-mgmt-app/src/app/users/services/user.service.spec.ts
+++ user-mgmt-app/src/app/users/services/user.service.spec.ts
@@ -1,16 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { User } from '../domain/user.domain';
 
 import { UserService } from './user.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('UserService', () => {
+fdescribe('UserService', () => {
   let service: UserService;
 
+  let httpClient: HttpClient;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: []
+    });
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should get data', () => {
+    const expected: User[] = [
+      {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        login: 'login',
+        password: 'password'
+      }
+    ];
+
+    const spy = spyOn(httpClient, 'get').and.returnValue(of([...expected]));
+    service.get().subscribe(results => {
+      expect(results).toEqual(expected);
+    })
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
